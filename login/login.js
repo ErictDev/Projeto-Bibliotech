cc32b5c51201fbc083df45dcee8f890f72c182ba
ERIC TSURUTA <55429123897@info.local>
2026-09-04 17:27:34 -0300

    inicio de java

---
+++ login/login.js
@@ -0,0 +1,11 @@
+const campoSenha = document.getElementById("senha")
+const btnSenha = document.querySelector("#mostrar-senha")
+
+btnSenha.addEventListener("click", function() {
+    alert("Cliquei no botão")
+    })
+
+
+
+
+
