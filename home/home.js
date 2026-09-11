d6e10d4f61f1c3bc80488b8240ff4a14fca975ce
ERIC TSURUTA <55429123897@info.local>
2026-09-11 17:22:21 -0300

    :iphone: feat: responsividade concluida

---
+++ home/home.js
@@ -0,0 +1,16 @@
+const menu = document.getElementById("menu"); //Pegando o elemento pelo id 
+
+//estamos pegando o elemento de forma genérica - pode ser classe (precisa trazer o ponto) ou pode ser ID (precisa trazer a hashtag)
+const navMenu = document.querySelector(".navegacao");
+
+menu.addEventListener("click", function() {
+    if(navMenu.className == "navegacao") {
+        navMenu.className = "navegacao ativo"
+    }
+    else {
+        navMenu.className = "navegacao"
+    }
+})
+
+
+
